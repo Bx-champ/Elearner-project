8233987dc9448d9846904164984d5e8e51667305
Bx-champ <95280251+Bx-champ@users.noreply.github.com>
2025-08-05 10:22:29 +0530

    Update config.js
    new backend docker based

--- frontdesign/src/config.js
+++ frontdesign/src/config.js
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://elearner-project-1-0.onrender.com';
+// export const BASE_URL = 'https://elearner-project-1-0.onrender.com';
+//export const BASE_URL = 'http://localhost:5000'; // Local development URL
+export const BASE_URL = 'https://elearner-project-new-one.onrender.com';
