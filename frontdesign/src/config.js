b82a43a5f790a2a1e6ece8f6e408c0514090a366
Bx-champ <basantchamp1@gmail.com>
2025-10-02 08:31:03 +0530

    final config configure

--- frontdesign/src/config.js
+++ frontdesign/src/config.js
@@ -1,3 +1,4 @@
 // export const BASE_URL = 'https://elearner-project-1-0.onrender.com';
-//export const BASE_URL = 'http://localhost:5000'; // Local development URL
-export const BASE_URL = 'https://elearner-project-new-one.onrender.com';
+// export const BASE_URL = 'http://localhost:5000'; // Local development URL
+// export const BASE_URL = 'https://elearner-project-new-one.onrender.com';
+export const BASE_URL = 'https://api.zenithile.com';
